900dc25135974891b1eebadc40ee13b805c7d46d
hayamaster <dnfxmfkdbzos@naver.com>
2024-02-17 00:27:00 +0900

    feat: shadnc 설치

--- src/pages/EventPage/index.jsx
+++ src/pages/EventPage/index.jsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button"
+
 const EventPage = () => {
   return (
     <div>
       <h1 className="bg-red-200">Event Page</h1>
+      <Button>버튼</Button>
     </div>
   )
 }
