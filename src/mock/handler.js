a79b873c06838962328909c77efa39d56cb2a238
한재혁 <hanjaehyeok@hanjaehyeog-ui-MacBookPro.local>
2024-02-17 06:18:31 +0900

    [Feat]이벤트 요청 테스트

--- src/mock/handler.js
+++ src/mock/handler.js
@@ -18,20 +18,21 @@ export const handlers = [
     }
   }),
 
-  rest.post("/create", (req, res, ctx)=>{
-    const {date,event_name} = req.body
-    if(date && event_name) {
+  rest.post("/create", (req, res, ctx) => {
+    const { date, event_name } = req.body
+    if (date.length !== 0 && event_name) {
       return res(
         ctx.status(200),
         ctx.json({
-          message:"이벤트 보내기 완료",
+          message: "이벤트 보내기 완료",
           date: date,
           event_name: event_name,
         })
       )
+    } else {
+      return res(ctx.status(400), ctx.json({ message: "이벤트 요청 실패" }))
     }
-  })
-
+  }),
 ]
 
 export default handlers
